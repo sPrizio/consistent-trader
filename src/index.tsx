6ac42ee8b2afbc36435a8da6235d08d297b043d7
sprizio <s.prizio@hotmail.com>
2023-06-29 10:31:59 -0400

    minor fixes

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />
     },
     {
-        path: "/report-issue",
+        path: "/report",
         element: <ContentPage pageId={'report'} />,
         errorElement: <ErrorPage />
     },
